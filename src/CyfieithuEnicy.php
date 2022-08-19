a0dc0edb9e9248111ddca0ef62d76e4058adf478
Ian Low <ianlow28@gmail.com>
2022-08-19 16:38:20 +0100

    Ymrwymiad cychwyniedig

---
+++ src/CyfieithuEnicy.php
@@ -0,0 +1,41 @@
+<?php
+/* ==============================================
+TO DO:
+======
+- cyfieithu-enicy - 
+   .jsonY dechrY seirchY igY git! github! pckgst!
+- ailgymmathu-geiriau-cy - 
+   .jsonY dechrY seirchY igY gitY
+- geiriadur-en-cy - 
+   .jsonY dechrY seirchY igY gitY
+- cymhwyso-treigliadau-cy
+   .jsonY dechrY seirchY igY gitY
+- poststatuscmd-teclynnau
+   .jsonY dechrY seirchY igY gitY
+-------------------------------------------------
+DONE:
+=====
+# Changelog
+## 1.0.0 - yyyy-mm-dd
+### Changed
+- details
+### Fixed
+- details
+### Added
+- details
+================================================= */
+namespace Ianl28\CyfieithuEnicy;
+class CyfieithuEnicy{
+
+  //-------------------------------
+  public function __construct(){
+    echo "This is called from CyfieithuEnicy->__construct()\n";
+  }//endfunc
+  //-------------------------------
+  public function testrun(){
+    echo "This is called from CyfieithuEnicy->testrun()\n";
+  }//endfunc
+  //-------------------------------
+
+}//endclass 
+?>
